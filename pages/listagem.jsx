26ca09656831862be4d66e79944bbb7ff0852c70
Guilherme Loschiavo Alvarez <61693858+AlvarezGui@users.noreply.github.com>
2022-08-03 08:48:26 -0300

    Add files via upload

---
+++ pages/listagem.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Listagem(){
+    return(
+        <>
+            <Head></Head>
+
+            <h1>Listagem dos Produtos</h1>
+        </>
+    );
+}
